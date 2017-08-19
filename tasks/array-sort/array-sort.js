34baa04ad97b585b620477858e428b43391ce285
RubaXa <k.lebedev@corp.mail.ru>
2017-08-19 14:14:21 +0300

    + array-sort

---
+++ tasks/array-sort/array-sort.js
@@ -0,0 +1,10 @@
+/**
+ * Релизуйте сортировку массива
+ * @param   {any[]}     values  сортируемый массив
+ * @returns {any[]}
+ */
+function sort(values) {
+	return values.sort()
+}
+
+console.log(sort([2, 3, 1, 4, 5, 7, 6])); // [1, 2, 3, 4, 5, 6]
