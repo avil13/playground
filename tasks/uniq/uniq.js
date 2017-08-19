586edb91d1b8a99619fa169154f937ca78f9db0d
RubaXa <k.lebedev@corp.mail.ru>
2017-08-19 11:44:46 +0300

    + uniq

---
+++ tasks/uniq/uniq.js
@@ -0,0 +1,12 @@
+/**
+ * Получения массива уникальных значений
+ * @param  {number[]} values
+ * @return {*}
+ */
+function uniq(values) {
+	return values;
+}
+
+const input = [2, 3, 1, 2, 1, 5, 6, 3, 1, 8, 5];
+
+console.log(uniq(input));
---
+++ tasks/uniq/uniq.tests.js
@@ -0,0 +1,5 @@
+({
+	"uniq(input)": function () {
+		return [uniq(input), [2, 3, 1, 5, 6, 8]];
+	}
+})
