613c39af539abf98dae03460d4ce07270bad4ae7
RubaXa <k.lebedev@corp.mail.ru>
2017-08-21 10:51:36 +0300

    + toArray

--- tasks/binary-tree/binary-tree.js
+++ tasks/binary-tree/binary-tree.js
@@ -7,7 +7,7 @@ function BinaryTree() {
 
 var tree = new BinaryTree();
 
-tree.add(1, 2, 3, 4);
+tree.add(2, 1, 3, 2.5, 4);
 tree.add(5);
 
 console.log('2:', tree.has(2)); // true
@@ -15,4 +15,8 @@ console.log('5:', tree.has(5)); // true
 
 tree.remove(3);
 
+console.log('3:', tree.has(3)); // false
+console.log('2.5:', tree.has(2.5)); // true
+
 console.log('size:', tree.size()); // 4
+console.log('toArray:', tree.toArray()); // [1, 2, 2.5, 4, 5]
