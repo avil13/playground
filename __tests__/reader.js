0f8c06474ffa091dd3e73c84b3ff1b2e6556a720
avil13 <13.avil@gmail.com>
2019-04-13 16:39:18 +0300

    wip

---
+++ __tests__/reader.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+function reader(dirPath) {
+    const name = path.basename(dirPath);
+
+    const codeLib = {
+        source: null,
+        testSrc: null,
+        solution: null
+    };
+
+    // fs.re
+}
