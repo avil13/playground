4ae22bc62a5e53d60227b46030d7a1c47749ebeb
avil13 <13.avil@gmail.com>
2019-04-15 14:34:32 +0300

    reader

--- __tests__/reader.js
+++ __tests__/reader.js
@@ -1,14 +1,49 @@
 const fs = require('fs');
 const path = require('path');
+const vm = require('vm');
+
+const FOLDER = path.join(__dirname, 'tasks/a-lt-a');
+
 
 function reader(dirPath) {
     const name = path.basename(dirPath);
 
     const codeLib = {
-        source: null,
-        testSrc: null,
-        solution: null
+        source: {
+            src: '',
+            ext: 'js'
+        },
+        testSrc: {
+            src: '',
+            ext: 'tests.js'
+        },
+        solution: {
+            src: '',
+            path: 'solution/index.js'
+        }
     };
 
-    // fs.re
+    for (var key in codeLib) {
+        if (codeLib.hasOwnProperty(key)) {
+            const el = codeLib[key];
+
+            const filename = el.path || el.ext && `${name}.${el.ext}`;
+        }
+
+        el.src = fs.readFileSync(path.join(dirPath, filename), 'utf-8');
+    }
+
+    return codeLib;
 }
+
+
+
+
+
+
+
+
+
+
+
+
