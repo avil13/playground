d53b5686544c5d6585433cd998dc60fb40fcd0e0
avil13 <13.avil@gmail.com>
2019-05-06 02:06:38 +0300

    [task] nested-set

--- tasks/nested-set/nested-set.tests.js
+++ tasks/nested-set/nested-set.tests.js
@@ -1,3 +1,8 @@
 ({
-
+    "container.innerHTML ...check?": function () {
+        return [
+            container.innerHTML,
+            '<ul><li>Одежда<ul><li>Мужская<ul><li>Костюмы<ul><li>Брюки</li></ul><ul><li>Жакеты</li></ul></li></ul><ul><li>Брюки</li></ul><ul><li>Жакеты</li></ul></li></ul><ul><li>Женская<ul><li>Платья<ul><li>Вечерние</li></ul><ul><li>Летние</li></ul></li></ul><ul><li>Юбки</li></ul><ul><li>Блузы</li></ul><ul><li>Вечерние</li></ul><ul><li>Летние</li></ul></li></ul><ul><li>Костюмы<ul><li>Брюки</li></ul><ul><li>Жакеты</li></ul></li></ul><ul><li>Платья<ul><li>Вечерние</li></ul><ul><li>Летние</li></ul></li></ul></li></ul>'
+        ];
+    }
 })
