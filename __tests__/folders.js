850efd126d4fc2bfd29030e97762b557a7277659
avil13 <13.avil@gmail.com>
2019-05-21 16:24:20 +0300

    solution folder file

---
+++ __tests__/folders.js
@@ -0,0 +1,52 @@
+/**
+ * Список папок в которых нужно проверять решение
+ * в dev режиме, из списка берется только посленяя запись
+ */
+const FOLDERS = [
+    'a-lt-a',
+    'anagrams',
+    'argument',
+    'array-missing',
+    'array-sort',
+    'array.size',
+    'async-pause',
+    'async-reduce',
+    'attr-accessor',
+    'binary-tree',
+    'bomb',
+    'calc',
+    'class-name',
+    'clicks',
+    'constructor', // тут беда с описанием задачи
+    'deferred',
+    'delay',
+    'dropdown',
+    'dummy',
+    'extend',
+    'futures',
+    'get-range',
+    'graph',
+    'intersection',
+    'is-balanced',
+    'is-isomorphic',
+    'is-palindrome',
+    'minmax',
+    'naturalnumber',
+    'nested-set',
+    'parallel',
+    'printnumbers',
+    // react-vs-iframe ???
+    'regexp',
+    'rle',
+    'romannumbers',
+    'rpn',
+    'safehtml',
+    'sum',
+    'uniq',
+    'waterfall',
+    'xmap',
+    'zsort',
+    // ''
+];
+
+module.exports = FOLDERS;
